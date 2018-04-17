e83acbda871940ec6b1c0be813ffdb953ce5b8c3
Mykyta <nikita1opanyuk@gmail.com>
2018-04-17 17:51:30 +0300

    Also continue writting a client and start writting a server.

--- lab_3_TCPserver_chat_Qt/TCP_v2_client/mainwindow.cpp
+++ lab_3_TCPserver_chat_Qt/TCP_v2_client/mainwindow.cpp
@@ -18,9 +18,82 @@ MainWindow::MainWindow(QWidget *parent) :
         if (!domain.isEmpty())
             ui->comboBox->addItem(name + QChar('.') + domain);
     }
+
+    tcpSocket = new QTcpSocket(this);
+
+    connect(hostCombo, SIGNAL(editTextChanged(QString)),
+            this, SLOT(enableGetFortuneButton()));
+    connect(portLineEdit, SIGNAL(textChanged(QString)),
+            this, SLOT(enableGetFortuneButton()));
+    connect(getFortuneButton, SIGNAL(clicked()),
+            this, SLOT(requestNewFortune()));
+    connect(quitButton, SIGNAL(clicked()), this, SLOT(close()));
+
+    connect(tcpSocket, SIGNAL(readyRead()), this, SLOT(read_text()));
+
+    connect(tcpSocket, SIGNAL(error(QAbstractSocket::SocketError)),
+        this, SLOT(displayError(QAbstractSocket::SocketError)));
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::displayError(QAbstractSocket::SocketError socketError)
+{
+    switch (socketError) {
+    case QAbstractSocket::RemoteHostClosedError:
+        break;
+    case QAbstractSocket::HostNotFoundError:
+        QMessageBox::information(this, tr("Chat"),
+                                 tr("The host was not found. Please check the "
+                                    "host name and port settings."));
+        break;
+    case QAbstractSocket::ConnectionRefusedError:
+        QMessageBox::information(this, tr("Chat"),
+                                 tr("The connection was refused by the peer. "
+                                    "Make sure the fortune server is running, "
+                                    "and check that the host name and port "
+                                    "settings are correct."));
+        break;
+    default:
+        QMessageBox::information(this, tr("Chat"),
+                                 tr("The following error occurred: %1.")
+                                 .arg(tcpSocket->errorString()));
+    }
+
+    ui->Send->setEnabled(true);
+    ui->Send_file->setEnabled(true);
+}
+void MainWindow::enableGetFortuneButton()
+{
+    getFortuneButton->setEnabled((!networkSession || networkSession->isOpen()) &&
+                                 !hostCombo->currentText().isEmpty() &&
+                                 !portLineEdit->text().isEmpty());
+
+}
+
+void MainWindow::sessionOpened()
+{
+    // Save the used configuration
+    QNetworkConfiguration config = networkSession->configuration();
+    QString id;
+    if (config.type() == QNetworkConfiguration::UserChoice)
+        id = networkSession->sessionProperty(QLatin1String("UserChoiceConfiguration")).toString();
+    else
+        id = config.identifier();
+
+    QSettings settings(QSettings::UserScope, QLatin1String("QtProject"));
+    settings.beginGroup(QLatin1String("QtNetwork"));
+    settings.setValue(QLatin1String("DefaultNetworkConfiguration"), id);
+    settings.endGroup();
+
+    statusLabel->setText(tr("This examples requires that you run the "
+                            "Fortune Server example as well."));
+
+    enableGetFortuneButton();
+}
+
